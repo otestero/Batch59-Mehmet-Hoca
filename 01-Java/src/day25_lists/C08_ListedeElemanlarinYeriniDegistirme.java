776186730e5acdb65098db750bce90ba4a9711ee
bulutluoz <abbulutluoz@gmail.com>
2022-03-24 21:35:23 +0100

    26.gun sonu

--- 01-Java/src/day25_lists/C08_ListedeElemanlarinYeriniDegistirme.java
+++ 01-Java/src/day25_lists/C08_ListedeElemanlarinYeriniDegistirme.java
@@ -20,15 +20,24 @@ public class C08_ListedeElemanlarinYeriniDegistirme {
 
         int ilkIndex=2;
         int ikinciIndex=5;
-
+        System.out.println("swap'dan once list : " + sayilar); // bu satir gereksiz, gormeniz icin yazdim
         sayilar= swapElements(sayilar,ilkIndex,ikinciIndex);
+        System.out.println("swap'dan sonra list : " + sayilar); // bu satir gereksiz, gormeniz icin yazdim
     }
 
     public static List<Integer> swapElements(List<Integer> sayilar, int ilkIndex, int ikinciIndex) {
             // bir tem sayi olusturup
             // verilen indexlerdeki sayilari yer degistirin
             // indexleri kontrol edip sinirin otesinde index verildiyse uyari mesaji yazdirin
-
+        int temp = 0;
+        if ((ilkIndex > sayilar.size() - 1) || (ikinciIndex > sayilar.size() - 1)
+             || ilkIndex<0 || ikinciIndex<0) {
+            System.out.println("Lutfen Gecerli bir index giriniz");
+        } else {
+            temp=sayilar.get(ilkIndex);
+            sayilar.set(ilkIndex,sayilar.get(ikinciIndex));
+            sayilar.set(ikinciIndex,temp);
+        }
 
         return sayilar;
     }

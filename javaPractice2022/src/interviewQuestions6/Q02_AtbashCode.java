cb0055fb8b21d27f590a8b37bc667350471cc5b8
Des_Abd_Wis <destiniyabidingwisdom@gmail.com>
2022-04-18 15:42:40 +0300

    18.04.22

---
+++ javaPractice2022/src/interviewQuestions6/Q02_AtbashCode.java
@@ -0,0 +1,10 @@
+package interviewQuestions6;
+
+public class Q02_AtbashCode {
+    /*
+      AtbashCode ; bir metni şifrelemek veya şifresini çözmek için İbrani alfabesine temelli basit bir yöntemdir.
+      "abcdefghijklmnopqrstuvwxyz"; 26 harf
+      "zyxwvutsrqponmlkjihgfedcba";
+     */
+
+}

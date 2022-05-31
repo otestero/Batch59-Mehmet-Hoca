3536fae69d4ad7a2cd13a03ef51e1e05707cb70c
bulutluoz <abbulutluoz@gmail.com>
2022-05-31 22:31:00 +0200

    19.gun sonu

--- Selenium/com.TestNG_Batch59/src/test/java/utilities/Driver.java
+++ Selenium/com.TestNG_Batch59/src/test/java/utilities/Driver.java
@@ -16,7 +16,21 @@ public class Driver {
     Driver class'indan static method'lar kullanarak
     driver olusturup, ilgili ayarlarin yapilmasi
     ve en sonda driver'in kapatilmasi tercih edilmistir.
+
+    POM'de Driver class'indaki getDriver()'nin obje olusturularak kullanilmasini
+    engellemek icin
+    Singleton pattern kullanimi benimsenmistir.
+
+    Singleton Pattern : tekli kullanim, bir class'in farkli class'lardan
+    obje olusturularak kullanimini engellemek icin kullanilir.
+
+    Bunu saglamak icin yapmamiz gereken sey oldukca basit
+    obje olusturmak icin kullanilan constructor'i private yaptiginizda
+    bsaka class'larda Driver class'indan obje olusturulmasi mumkun OLAMAZ
      */
+    private  Driver(){
+
+    }
     static WebDriver driver;
 
     public static WebDriver getDriver(){

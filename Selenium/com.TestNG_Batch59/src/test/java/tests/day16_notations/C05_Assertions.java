b15567d441b1aaf374245e01b67b761bfdaaf576
bulutluoz <abbulutluoz@gmail.com>
2022-06-03 09:36:50 +0200

    21.gun sonu

--- Selenium/com.TestNG_Batch59/src/test/java/tests/day16_notations/C05_Assertions.java
+++ Selenium/com.TestNG_Batch59/src/test/java/tests/day16_notations/C05_Assertions.java
@@ -15,7 +15,7 @@ public class C05_Assertions extends TestBase {
         // 1- amazon anasayfaya gidin
         driver.get("https://www.amazon.com");
         // 2- title'in Amazon icerdigini test edin
-        String expectedTitle="amazon";
+        String expectedTitle="Amazon";
         String actualTitle= driver.getTitle();
         Assert.assertTrue(actualTitle.contains(expectedTitle));
         // 3- Arama kutusunun erisilebilir oldugunu test edin
@@ -27,7 +27,7 @@ public class C05_Assertions extends TestBase {
         WebElement sonucYaziElementi= driver.findElement(By.xpath("//div[@class='a-section a-spacing-small a-spacing-top-small']"));
         Assert.assertTrue(sonucYaziElementi.isDisplayed());
         // 6- arama sonucunun Nutella icerdigini test edin
-        Assert.assertTrue(sonucYaziElementi.getText().contains("Kutella"));
+        Assert.assertTrue(sonucYaziElementi.getText().contains("Nutella"));
 
     }
 }
